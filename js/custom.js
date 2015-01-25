233a5b1fe71954d82975fb570b66df03db8fa697
Seth Rubin <seth.h.rubin@gmail.com>
2015-01-25 15:16:11 -0500

    finish animation, clean up code, make font nicer, switch index.html line ending to unix, switch everything to cdn

--- js/custom.js
+++ js/custom.js
@@ -6,20 +6,23 @@ $(document).ready(function(){
 });
 
 function animate(){
-	var images = ["img/ph1.png", "img/ph2.jpg", "img/ph3.jpeg", "img/ph4.jpg", "img/ph1.png", "img/ph2.jpg"]
-	var top = [5, 55, 30, 0, 60, 35];
+	var images = ["img/ph1.png", "img/ph2.jpg", "img/ph3.jpeg", "img/ph4.jpg", "img/ph1.png", "img/ph2.jpg"];
+	var top = [5, 50, 25, 0, 55, 30];
 	var delay = 0;
-	$("div.overlay").delay(1000).fadeTo(1000, 0.5);
+
+	// fade out screen
+	$("div.overlay").delay(1000).fadeIn(1000);
+
+	// slide images on screen
 	$.each(images, function(index, src){
 		var image = $("<img>").attr("src", src).addClass("animate").appendTo("body")
 		                      .css("top", top[index % top.length] + (Math.random() * 20 - 10) + "%");
-		var properties = { opacity: 1.5 };
 		var movement = 40 + (Math.random() * 30 - 15);
 		if (index % 2) {
-			$.extend(properties, { left: "+=" + movement + "%" });
+			var properties = { left: "+=" + movement + "%", opacity: 1.5 };
 			image.css("left", "-25%");
 		} else {
-			$.extend(properties, { right: "+=" + movement + "%" });
+			var properties =  { right: "+=" + movement + "%", opacity: 1.5 };
 			image.css("right", "-25%");
 		}
 		setTimeout(function(){
@@ -27,31 +30,30 @@ function animate(){
 		}, delay + 2000);
 		delay += 1000;
 	});
-	var side = true;
-	$("img.animate").each(function(){
-		var image = $(this);
+
+	// images fly off screen
+	$("img.animate").each(function(index, image){
 		var num1 = Math.random() * 500 - 250;
 		num1 = num1 >= 0 ? num1 + 100 : num1 - 100;
 		var num2 = Math.random() * 500 - 250;
 		num2 = num2 >= 0 ? num2 + 100 : num2 - 100;
-		if (side) {
-			var properties = {right: num1 + "%", top: num2 + "%"};
-		} else {
-			var properties = {left: num1 + "%", top: num2 + "%"};
-		}
-		side = !side;
+		var properties = index % 2 ? {left: num1 + "%", top: num2 + "%"} : {right: num1 + "%", top: num2 + "%"};
 		setTimeout(function(){
-			image.animate(properties, 2500);
+			$(image).animate(properties, 2000);
 			setTimeout(function(){
-				image.remove();
-			}, 2500);
+				$(image).remove();
+			}, 2000);
 		}, delay + 7000);
 		delay += 20;
 	});
-	var div = $("<div>").addClass("name").appendTo("body").html("<h1>Seth Rubin</h1><h3>Software Developer</h3>");
-	div.delay(delay + 9000).fadeIn(1000).delay(2000).fadeOut(1000);
+
+	// show name
+	var div = $("<div>").addClass("name").appendTo("body").html("<h1 class='name'>Seth Rubin</h1><h3 class='name'>Software Developer</h3>");
+	div.delay(delay + 7000).fadeIn(1000).delay(3000).fadeOut(1000);
 	setTimeout(function(){
 		div.remove();
-	}, delay + 13000);
-	$("div.overlay").delay(delay + 12500).fadeOut(1000);
+	}, delay + 12000);
+
+	// fade in screen
+	$("div.overlay").delay(delay + 11500).fadeOut(1000);
 }
